f3a538838f5122bb6d86bec332946fe4d7342f7a
近藤元気 <genki7onepiece@gmail.com>
2020-05-05 14:59:53 +0900

    Initial Commit

---
+++ IWannaGo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IWannaGo
+//
+//  Created by 近藤元気 on 2020/05/05.
+//  Copyright © 2020 genkikondo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
